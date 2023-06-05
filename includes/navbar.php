02d02a0090eba0c024bbcf51d93067e648d86afa
dannycornelio <licdannycornelio@gmail.com>
2023-06-04 19:05:04 -0600

    combios del navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -4,22 +4,34 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <a class="navbar-brand" href="#">Tienda virtual  Clientes</a>
+            <a class="navbar-brand" href="#">Tienda virtual</a>
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
                 </li>
-              
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Clientes
+                        Ventas
                     </a>
                     <ul class="dropdown-menu bg-dark">
                         <li class="nav-item">
-                            <a class="nav-link" href="/practica_9/vistas/clientes/index.php">Agregar Cliente</a>
+                            <a class="nav-link" href="/crudphp18may2023/vistas/ventas/index.php">Ingresar venta</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/practica_9/vistas/clientes/buscar.php">Buscar Cliente</a>
+                            <a class="nav-link" href="/crudphp18may2023/vistas/ventas/buscar.php">Buscar venta</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Productos
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/crudphp18may2023/vistas/productos/index.php">Crear producto</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/crudphp18may2023/vistas/productos/buscar.php">Buscar producto</a>
                         </li>
                     </ul>
                 </li>
